02c19599641558b78b7c362476010dd8c2272a53
Karol Janowski <karoljanowski@yahoo.com>
2025-02-25 13:06:13 +0100

    added InputDateCache class to read data from car-dealership-traffic-simulation.md to simulate frontend layer

---
+++ src/main/java/pl/zajavka/business/management/InputDataCache.java
@@ -0,0 +1,44 @@
+package pl.zajavka.business.management;
+
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class InputDataCache {
+
+    private static final String FILE_PATH = "./src/main/resources/car-dealership-traffic-simulation.md";
+
+    @Getter
+    private static final Map<String, List<String>> inputData;
+
+    static {
+        try {
+            inputData = readFileContent();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static Map<String, List<String>> readFileContent() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH)).stream()
+                .filter(line -> !line.startsWith("[//]: #"))
+                .filter(line -> !line.isBlank())
+                .toList();
+
+        return lines.stream().collect(Collectors.groupingBy(
+                line -> line.split("->")[0].trim(),
+                Collectors.mapping(
+                        line -> line.substring(line.indexOf("->") + 2).trim(),
+                        Collectors.toList()
+                )
+        ));
+    }
+
+}
